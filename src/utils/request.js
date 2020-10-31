f27794ce248adbf9a5375839d76a9d706a7b0e0b
Fate <bboyfate@163.com>
2020-10-31 14:57:33 +0800

    修改域名

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,9 +3,11 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
+
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_URL + process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
